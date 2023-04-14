1343b3a504fd78da64da22665338e48b8f90fe81
Kevin Stiven Moreno Martinez <kmorenom1@ucentral.edu.co>
2023-04-14 02:36:58 +0000

    Adding the qualitative variables analys

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -1,8 +1,11 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType,FloatType
+from pyspark.sql.functions import col, udf
 import boto3
 import os
+import pandas as pd
 from dotenv import load_dotenv
 
 
@@ -42,4 +45,31 @@ Key = files['Contents'][0]['Key']
 path = f"s3a://{bucket}/{Key}"
 
 s3_df = spark.read.option('header', True).csv(path)
-s3_df.show()
+
+
+s3_df.printSchema()
+
+
+def clean_values(value):
+  return value.split(' ')[0]
+
+
+clean_values_udf = udf(lambda z : clean_values(z), StringType())
+sql_context.udf.register('clean_values', clean_values_udf)
+
+s3_df = s3_df.select('DEPARTAMENTO', clean_values_udf('MUNICIPIO').alias('MUNICIPIO'), 'CODIGO DANE', 'ARMAS MEDIOS', 'FECHA HECHO', 'GENERO', 'GRUPO ETARIO', 'TIPO DE HURTO', 'CANTIDAD')
+
+
+
+s3_df.toPandas().describe().transpose()
+
+# Department with higher theft
+#s3_df.groupBy('DEPARTAMENTO').count().sort(col('count').desc()).show()
+
+# Township with higher theft
+#s3_df.groupBy('MUNICIPIO').count().orderBy('count', ascending=False).show()
+# Weapon more frecuency
+
+# Type theft with more frecuency
+
+# Anything with date
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType,FloatType
+from pyspark.sql.functions import col, udf
 import boto3
 import os
+import pandas as pd
 from dotenv import load_dotenv
 
 
@@ -27,6 +30,8 @@ sc._jsc.hadoopConfiguration().set('fs.s3a.impl', 'org.apache.hadoop.fs.s3a.S3AFi
 
 spark=SparkSession(sc)
 
+
+
 s3 = boto3.client(
     's3',
     aws_access_key_id = os.getenv('aws_access_key_id'),
@@ -42,4 +47,31 @@ Key = files['Contents'][0]['Key']
 path = f"s3a://{bucket}/{Key}"
 
 s3_df = spark.read.option('header', True).csv(path)
-s3_df.show()
+
+
+s3_df.printSchema()
+
+
+def clean_values(value):
+  return value.split(' ')[0]
+
+
+clean_values_udf = udf(lambda z : clean_values(z), StringType())
+sql_context.udf.register('clean_values', clean_values_udf)
+
+s3_df = s3_df.select('DEPARTAMENTO', clean_values_udf('MUNICIPIO').alias('MUNICIPIO'), 'CODIGO DANE', 'ARMAS MEDIOS', 'FECHA HECHO', 'GENERO', 'GRUPO ETARIO', 'TIPO DE HURTO', 'CANTIDAD')
+
+
+
+s3_df.toPandas().describe().transpose()
+
+# Department with higher theft
+#s3_df.groupBy('DEPARTAMENTO').count().sort(col('count').desc()).show()
+
+# Township with higher theft
+#s3_df.groupBy('MUNICIPIO').count().orderBy('count', ascending=False).show()
+# Weapon more frecuency
+
+# Type theft with more frecuency
+
+# Anything with date
